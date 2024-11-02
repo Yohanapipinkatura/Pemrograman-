6a79c052629a05f4254bdff9d8e344506e1ae1ca
Yohanapipinkatura <yohanapipinkatura@gmail.com>
2024-11-02 12:44:29 +0700

    Add files via upload

---
+++ BMIBeraksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pemerograman;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BMIBeraksi {
+    /**
+     * @parm args the command line arguments
+     */
+    public static void main(String[] args) {
+        BMI yess = new BMI (45,1.40);
+        yess.berat();
+        yess.hasil();
+                
+                
+                
+                
+    }
+    
+}
